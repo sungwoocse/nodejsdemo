89688bb4a0dd80667caa1aa3dc50600689980de2
sungwoocse <sungwoocse@gmail.com>
2024-02-14 12:27:38 +0900

    node study 1.

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+
+app.get("/", function (req, res) {
+  res.send(
+    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>'
+  );
+});
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  console.log(userName);
+  res.send("<h1>Username stored!</h1>");
+});
+
+app.listen(3000);
