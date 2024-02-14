d21d6d5a8590141bd1c5669cd8e66cfc128c56a4
sungwoocse <sungwoocse@gmail.com>
2024-02-14 21:15:40 +0900

    day 48 finish

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
+const express = require("express");
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
@@ -8,14 +12,38 @@ app.get("/currenttime", function (req, res) {
 
 app.get("/", function (req, res) {
   res.send(
-    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>'
+    '<form action="/store-user" method="POST"><label>Your Name:</label><input type="text" name="username"><button>Submit</button></form>'
   );
 });
 
 app.post("/store-user", function (req, res) {
   const userName = req.body.username;
-  console.log(userName);
+
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  existingUsers.push(userName);
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
   res.send("<h1>Username stored!</h1>");
+});
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = "<ul>";
+
+  for (const user of existingUsers) {
+    responseData += "<li>" + user + "</li>";
+  }
+
+  responseData += "</ul>";
+
+  res.send(existingUsers);
 });
 
 app.listen(3000);
